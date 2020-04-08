d85fe42ed4fd8f437f0fc6c605497be4f1832274
Daniel André Schneider <dani@mgs-software.de>
2020-04-08 03:27:00 +0200

    Initial commit, empty .NET Core Web App

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Schafkopf.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
